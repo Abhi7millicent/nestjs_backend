ea5f769c977f1db434bdff622fae99b97a4ec2ea
abhishek@millicent.in <abhishek@millicent.in>
2024-06-01 20:29:39 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   dotenv.config(); // Load environment variables from .env file
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT); 
+  const port = process.env.PORT || 3000; // Default to port 3000 if not specified in .env
+  await app.listen(port); 
 }
 bootstrap();
